ba53dcc8a74ec36fe73f11c7c3da7dcda1cd63b1
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-07-29 21:00:09 +0300

    Create getNumberOfNotesByCategory.ts

---
+++ src/utilities/getNumberOfNotesByCategory.ts
@@ -0,0 +1,5 @@
+import { Note } from '../store';
+
+export function getNumberOfNotesByCategory(notes: Note[], category: string, archived: boolean) {
+    return notes.filter((note) => note.category === category && note.archived === archived).length;
+};
