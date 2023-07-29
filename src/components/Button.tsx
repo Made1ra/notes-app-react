7a572fff699f755abe61e16ef1e466cf9bf65f20
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-07-29 11:48:14 +0300

    Create Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    cursor: pointer;
+    border: none;
+    padding: 1em;
+    background-color: #d3d3d3;
+    font-weight: bold;
+
+    &:hover {
+        opacity: 0.9;
+    }
+`;
+
+type ButtonProps = {
+    children?: React.ReactNode;
+    onClick?: () => void;
+};
+
+
+function Button({ children }: ButtonProps) {
+    return (
+        <StyledButton>{children}</StyledButton>
+    );
+}
+
+export default Button;
